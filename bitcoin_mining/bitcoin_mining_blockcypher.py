12c8b2e5327c9c15797643f0a99be14a96c1f916
frankgreg <f.gregoire.3@gmail.com>
2020-09-10 08:21:34 -0400

    asdf

--- bitcoin_mining/bitcoin_mining_blockcypher.py
+++ bitcoin_mining/bitcoin_mining_blockcypher.py
@@ -20,7 +20,7 @@ def elapsed_time(function):
         start = time.perf_counter()
         function(*args, **kwargs)
         stop = time.perf_counter()
-        print(f'Temps écoulé = {stop - start} s')
+        print(f'Temps écoulé = {stop - start:.5f} s')
 
     return calculate_elapsed_time
 
@@ -32,19 +32,18 @@ def block_mining(block_data):
     merkle_root = convert_hex_to_reversed_hex(block_data['mrkl_root'])
     timestamp = convert_datetime_to_reversed_hex(block_data['time'], 'x')
     bits = convert_integer_to_reversed_hex(block_data['bits'], 'x')
+    target = calculate_target(block_data['bits'])
     block_nonce = block_data['nonce']
-    difficulty = '0000000000000000000fffffffffffffffffffffffffffffffffffffffffffff'
 
-    nonce_guess = block_nonce - 10000
+    nonce_guess = block_nonce - 100000
     counter = 1
     while nonce_guess >= 0:
         nonce = convert_integer_to_reversed_hex(nonce_guess, '08x')
         header_hex = version + previous_block + merkle_root + timestamp + bits + nonce
         header_bin = binascii.a2b_hex(header_hex)
         hash_byte = hashlib.sha256(hashlib.sha256(header_bin).digest()).digest()
-        reversed_hash_byte = hash_byte[::-1]
-        hash_hex = reversed_hash_byte.hex()
-        if int(hash_hex, 16) <= int(difficulty, 16):
+        hash_int = int.from_bytes(hash_byte, byteorder='little')
+        if hash_int <= target:
             print(
                 f'Nonce officiel : {block_nonce}\n'
                 f'Nonce trouvé: {nonce_guess}\n'
@@ -54,6 +53,15 @@ def block_mining(block_data):
             break
         nonce_guess += 1
         counter += 1
+
+
+def calculate_target(bits):
+    bits_hex = format(bits, 'x')
+    index = int(bits_hex[:2], 16)
+    coefficient = int(bits_hex[2:], 16)
+    target = coefficient*2**(8*(index-3))
+
+    return target
 
 
 def convert_datetime_to_reversed_hex(datetime_, format_spec):
@@ -79,7 +87,7 @@ def convert_hex_to_reversed_hex(hex_):
 
 
 if __name__ == '__main__':
-    url = 'https://btc.com/00000000000000000006adad23e5d52bf49614e796f6dc694b13af2d7f05a32f'
-    # url = 'https://btc.com/0000000000000000000ca006e5d26bb2ad04bfe5638af89dd8231c8208aa10e6'
+    # url = 'https://btc.com/00000000000000000006adad23e5d52bf49614e796f6dc694b13af2d7f05a32f'
+    url = 'https://btc.com/0000000000000000000ca006e5d26bb2ad04bfe5638af89dd8231c8208aa10e6'
     BLOCK_HASH = url.split('/')[-1]
     main(BLOCK_HASH)
